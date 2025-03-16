405a6dca541d9a035bf0663d2e5fb854079b77f0
nycollas42 <nycollaseduardo4242@gmail.com>
2025-03-16 19:30:20 -0300

    Desenvolver função adicionar

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 let valorTotal = document.getElementById('valor-total');
 let quantidade = document.getElementById('quantidade');
-let lista = document.getElementById('lista-produtos');
+let listaProdutos = document.getElementById('lista-produtos');
+let somatorio = 0;
 
 function adicionar() {
-    
+    let produto = document.getElementById('produto');
+    let total = 0;
+    let molde = '';
+    if (produto.value == 'Fone de ouvido - R$100') {
+        total = 100 * quantidade.value;
+        molde = `${quantidade.value}x ${produto.value}<br>`;
+    }
+    else if (produto.value == 'Celular - R$1400') {
+        total = 1400 * quantidade.value;
+        molde = `${quantidade.value}x ${produto.value}<br>`;
+    }
+    else if (produto.value == 'Oculus VR - R$5000') {
+        total = 5000 * quantidade.value;
+        molde = `${quantidade.value}x ${produto.value}<br>`;
+    }
+    listaProdutos.innerHTML += molde;
+    somatorio += total
+    valorTotal.innerHTML = `R$${somatorio}`;
 }
 
 function limpar() {
     valorTotal.innerHTML = 'R$0'
     quantidade.value = 0;
-    lista.innerHTML = '';
+    listaProdutos.innerHTML = '';
+    somatorio = 0;
 }
