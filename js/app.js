ddafab491ab10e42c5d24583875bddbd956e2479
nycollas42 <nycollaseduardo4242@gmail.com>
2025-03-16 17:54:25 -0300

    Adicionar função limpar

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let valorTotal = document.getElementById('valor-total');
+let quantidade = document.getElementById('quantidade');
+let lista = document.getElementById('lista-produtos');
+
+function adicionar() {
+    
+}
+
+function limpar() {
+    valorTotal.innerHTML = 'R$0'
+    quantidade.value = 0;
+    lista.innerHTML = '';
+}
